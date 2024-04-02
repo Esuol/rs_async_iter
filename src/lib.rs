b4a2bf50944b8a43a28707e7a3aa9d48f3b2da38
berlinen <1292393108@qq.com>
2024-04-02 15:10:40 +0800

    feat: impl FromIterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(incomplete_features)]
+#![feature(return_position_impl_trait_in_trait)]
+#![feature(async_fn_in_trait)]
+#![forbid(unsafe_code, future_incompatible)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs)]
+
+mod from_iterator;
 mod into_iterator;
 mod iter;
 
 pub use crate::iter::{Iterator, Map};
+pub use from_iterator::FromIterator;
 pub use into_iterator::IntoIterator;
